e26400dac7ef62a2e934afadcee4f5cad910bd6c
Tsvetkov Maxim <pushiniiist@gmail.com>
2025-02-13 00:36:56 +0200

    use regexp instead of strings

--- internal/handler/text.go
+++ internal/handler/text.go
@@ -1,24 +1,30 @@
 package handler
 
 import (
-	"strings"
+	"regexp"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/pushinist/telegram-bot/internal/model"
 )
 
 type TextHandler struct {
-	triggers map[string]string
+	// triggers map[string]string
+	triggers map[*regexp.Regexp]string
 }
 
 func NewTextHandler() *TextHandler {
 	return &TextHandler{
-		triggers: map[string]string{
-			"разрабы дауны": "./assets/gifs/devs.gif",
-			"разраб даун":   "./assets/gifs/devs.gif",
-			"даун":          "./assets/gifs/devs.gif",
-			"йоу":           "./assets/gifs/cat-wif.gif",
+		triggers: map[*regexp.Regexp]string{
+			regexp.MustCompile(`(?i)(?:да[у]?н[а-я]*|разраб[а-я]*)`): "./assets/gifs/devs.gif",
+			regexp.MustCompile(`(?i)й[оО][уУ]?(?:\b|[^а-яА-Я]|$)`):   "./assets/gifs/cat-wif.gif",
 		},
+
+		// triggers: map[string]string{
+		// 	"разрабы дауны": "./assets/gifs/devs.gif",
+		// 	"разраб даун":   "./assets/gifs/devs.gif",
+		// 	"даун":          "./assets/gifs/devs.gif",
+		// 	"йоу":           "./assets/gifs/cat-wif.gif",
+		// },
 	}
 }
 
@@ -28,7 +34,7 @@ func (h *TextHandler) CanHandle(message *tgbotapi.Message) bool {
 
 func (h *TextHandler) Handle(task *model.MessageTask) error {
 	for trigger, gifPath := range h.triggers {
-		if strings.Contains(trigger, strings.ToLower(task.Message.Text)) {
+		if trigger.MatchString(task.Message.Text) {
 			return sendGifResponse(task.Bot, task.Message, gifPath)
 		}
 	}
